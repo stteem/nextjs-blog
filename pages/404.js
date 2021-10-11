49f61a36ed141d73c3b0eb8d98c5ab2efd0e7632
Uwem Uke <slyart147@gmail.com>
2021-10-11 04:37:22 +0100

    first commit

---
+++ pages/404.js
@@ -0,0 +1,10 @@
+import Layout from '../components/layout'
+
+
+export default function Custom404() {
+    return (
+        <Layout>
+            <h1>404 - Page Not Found Right About Now</h1>
+        </Layout>
+    )
+}
